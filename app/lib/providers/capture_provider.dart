24dc6033fe5a74827d728a78a0ceba1d5b1308c0
thịnh <ngocthinhdp@gmail.com>
2024-08-30 11:57:21 +0700

    Add post processing message event

--- app/lib/providers/capture_provider.dart
+++ app/lib/providers/capture_provider.dart
@@ -120,6 +120,26 @@ class CaptureProvider extends ChangeNotifier with WebSocketMixin, OpenGlassMixin
     _proessOnMemoryCreate(null); // force failed
   }
 
+  Future<void> _onMemoryPostProcessSuccess(String memoryId) async {
+    var memory = await getMemoryById(memoryId);
+    if (memory == null) {
+      print("Memory is not found $memoryId");
+      return;
+    }
+
+    memoryProvider?.updateMemory(memory);
+  }
+
+  Future<void> _onMemoryPostProcessFailed(String memoryId) async {
+    var memory = await getMemoryById(memoryId);
+    if (memory == null) {
+      print("Memory is not found $memoryId");
+      return;
+    }
+
+    memoryProvider?.updateMemory(memory);
+  }
+
   Future<bool?> _proessOnMemoryCreate(ServerMemory? memory) async {
     if (memory == null && (segments.isNotEmpty || photos.isNotEmpty)) {
       memory = ServerMemory(
@@ -318,12 +338,34 @@ class CaptureProvider extends ChangeNotifier with WebSocketMixin, OpenGlassMixin
         }
 
         if (event.type == MessageEventType.newMemoryCreated) {
+          if (event.memoryId == null) {
+            print("New memory created message event is invalid");
+            return;
+          }
           _onMemoryCreated(event.memoryId!);
           return;
         }
 
         if (event.type == MessageEventType.newMemoryCreateFailed) {
           _onMemoryCreateFailed();
+          return;
+        }
+
+        if (event.type == MessageEventType.memoryPostProcessingSuccess) {
+          if (event.memoryId == null) {
+            print("Post proccess message event is invalid");
+            return;
+          }
+          _onMemoryPostProcessSuccess(event.memoryId!);
+          return;
+        }
+
+        if (event.type == MessageEventType.memoryPostProcessingFailed) {
+          if (event.memoryId == null) {
+            print("Post proccess message event is invalid");
+            return;
+          }
+          _onMemoryPostProcessFailed(event.memoryId!);
           return;
         }
       },
